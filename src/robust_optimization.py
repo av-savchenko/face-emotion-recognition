a3b014ccc1c4efb7748fefe99e6c68838c4b0272
Andrey Savchenko <andrey.v.savchenko@gmail.com>
2022-05-04 16:36:36 +0300

    Fixed issue with incorrect training

--- src/robust_optimization.py
+++ src/robust_optimization.py
@@ -3,7 +3,7 @@ import torch
 
 class RobustOptimizer(torch.optim.Optimizer):
     def __init__(self, params, base_optimizer, eps=0.05, **kwargs):
-        defaults = dict(eps=eps, **kwargs)
+        defaults = dict(rho=eps, **kwargs)
         super(RobustOptimizer, self).__init__(params, defaults)
 
         self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
@@ -13,12 +13,12 @@ class RobustOptimizer(torch.optim.Optimizer):
     def first_step(self, zero_grad=False):
         grad_norm = self._grad_norm()
         for group in self.param_groups:
-            scale = group["eps"] / (grad_norm + 1e-12)
+            scale = group["rho"] / (grad_norm + 1e-12)
 
             for p in group["params"]:
                 if p.grad is None: continue
                 e_w = p.grad * scale.to(p)
-                p.add_(e_w)  # climb to the local maximum "w + e(w)"
+                p.add_(e_w)
                 self.state[p]["e_w"] = e_w
 
         if zero_grad: self.zero_grad()
@@ -28,22 +28,22 @@ class RobustOptimizer(torch.optim.Optimizer):
         for group in self.param_groups:
             for p in group["params"]:
                 if p.grad is None: continue
-                p.sub_(self.state[p]["e_w"])  # get back to "w" from "w + e(w)"
+                p.sub_(self.state[p]["e_w"])
 
-        self.base_optimizer.step()  # do the actual "sharpness-aware" update
+        self.base_optimizer.step()
 
         if zero_grad: self.zero_grad()
 
     @torch.no_grad()
     def step(self, closure=None):
-        closure = torch.enable_grad()(closure)  # the closure should do a full forward-backward pass
+        closure = torch.enable_grad()(closure)
 
         self.first_step(zero_grad=True)
         closure()
         self.second_step()
 
     def _grad_norm(self):
-        shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
+        shared_device = self.param_groups[0]["params"][0].device
         norm = torch.norm(
                     torch.stack([
                         p.grad.norm(p=2).to(shared_device)
