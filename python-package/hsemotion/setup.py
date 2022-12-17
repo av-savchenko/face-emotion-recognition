c1c0c30195f3a46f1f2de4f9876031afac7c555d
avsavchenko <andrey.v.savchenko@gmail.com>
2022-12-17 11:47:02 +0300

    Changed version of Python package to v0.2.1

--- python-package/hsemotion/setup.py
+++ python-package/hsemotion/setup.py
@@ -9,11 +9,12 @@ requirements = [
 ]
 setup(
     name='hsemotion',
-    version='0.2',
+    version='0.2.1',
     license='Apache-2.0',
     author="Andrey Savchenko",
     author_email='andrey.v.savchenko@gmail.com',
     packages=find_packages('.'),
+    download_url = 'https://github.com/HSE-asavchenko/face-emotion-recognition/archive/v0.2.1.tar.gz',
     url='https://github.com/HSE-asavchenko/face-emotion-recognition',
     description='HSEmotion Python Library for Facial Emotion Recognition',
     keywords='face emotion recognition',
--- python-package/hsemotion_onnx/setup.py
+++ python-package/hsemotion_onnx/setup.py
@@ -8,11 +8,12 @@ requirements = [
 ]
 setup(
     name='hsemotion_onnx',
-    version='0.3',
+    version='0.2.1',
     license='Apache-2.0',
     author="Andrey Savchenko",
     author_email='andrey.v.savchenko@gmail.com',
     packages=find_packages('.'),
+    download_url = 'https://github.com/HSE-asavchenko/face-emotion-recognition/archive/v0.2.1.tar.gz',
     url='https://github.com/HSE-asavchenko/face-emotion-recognition',
     description='HSEmotionONNX Python Library for Facial Emotion Recognition',
     keywords='face emotion recognition',
